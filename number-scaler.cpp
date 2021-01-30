2eb8871515e6b2588b28dee5710196df3f6ada73
Faizoo <faizaan3424@gmail.com>
2021-01-29 21:18:52 -0500

    working version

--- number-scaler.cpp
+++ number-scaler.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
 
+double multiplier(double inputNum, int scaler) {
+    return inputNum * scaler;
+}
+
 int main()
 {
+    //get user input
+    double inputNum{};
+    int scaler{};
+    std::cout << "What number would you like to multiply? ";
+    std::cin >> inputNum;
+    std::cout << std::endl << "What is your scaler? ";
+    std::cin >> scaler;
+    
+    //multiply input
+    double multipliedInput = multiplier(inputNum, scaler);
+
+    //print output
+    std::cout << "\n\nThe output is: " << multipliedInput;
     return 0;
 }
